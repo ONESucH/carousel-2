22e3cbc78c238de999e819a3c53b9177fa8f4f27
Onesuch <experts1994@mail.ru>
2017-01-21 16:47:04 +0300

    carousel#2

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var allImages = document.getElementsByClassName('all-images');
+
+function carousel(valueButton) {
+    var indent = 400;
+
+    switch (valueButton) {
+        case 'leftButton' :
+            break;
+        case 'rightButton' :
+            break;
+    }
+}
