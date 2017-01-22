28fc20f79dd06d751df4294e1dcff7ea1e5f713c
Onesuch <experts1994@mail.ru>
2017-01-22 11:43:28 +0300

    create animation for train block

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,30 @@
 'use strict';
 
-var allImages = document.getElementsByClassName('all-images');
+var allImageTrain = document.getElementsByClassName('all-images');
 
 function carousel(valueButton) {
-    var indent = 400;
-
     switch (valueButton) {
         case 'leftButton' :
+            var startLeftTrain = Date.now(),
+                timerLeftTrain = setInterval(function() {
+                var timePassed = Date.now() - startLeftTrain;
+
+                allImageTrain.style.left = timePassed / 5 + 'px';
+
+                if (timePassed > 2000) clearInterval(timerLeftTrain);
+
+            }, 20);
             break;
         case 'rightButton' :
+            var startRightTrain = Date.now(),
+                timerRightTrain = setInterval(function() {
+                    var timePassed = Date.now() - startRightTrain;
+
+                    allImageTrain.style.right = timePassed / 5 + 'px';
+
+                    if (timePassed > 2000) clearInterval(timerRightTrain);
+
+                }, 20);
             break;
     }
 }
